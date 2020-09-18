72526d9b309c64cb75cf11c5345837a6841df9c0
Guilherme H Noce <hidekinoce@gmail.com>
2020-09-18 00:45:48 -0300

    🌟 Challenge finalize

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,20 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type == 'outcome' && total < value) {
+      throw Error('You do not have enough balance');
+    }
+
+    const createTransaction = this.transactionsRepository.create({
+      title,
+      type,
+      value,
+    });
+
+    return createTransaction;
   }
 }
 
